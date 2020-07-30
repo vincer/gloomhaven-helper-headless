7edfa820403ae49a269505192d39bbf956381f2d
vince rosso <vincer@vincer.net>
2020-07-30 14:31:02 -0700

    Better Docker tagging

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
-FROM openjdk:14-slim-buster
+FROM openjdk:14.0.2-slim-buster
 
 RUN apt-get update
 RUN apt-get install --yes wget unzip xvfb liblwjgl-java tini
 
-ARG version=8.4.4
+# Version of Gloomhaven Helper to use, passed in as a build arg.
+# Latest version can be gleaned from http://esotericsoftware.com/gloomhaven-helper
+# e.g. 8.4.6 (no leading 'v')
+# GitHub Action set up to auto-fetch the latest version and build it.
+ARG version
 
 RUN wget http://esotericsoftware.com/files/ghh/GloomhavenHelper-$version.zip
 RUN unzip GloomhavenHelper-$version.zip
--- latest-gloomhaven-helper-action/version.js
+++ latest-gloomhaven-helper-action/version.js
@@ -10,8 +10,13 @@ async function run() {
 
         console.log(`Regex matched: ${found}`);
         const version = found[0].substr(1);
-        console.log(`Version: ${version}`);
+        const semver = version.match(/^([0-9]+)\.([0-9]+)\.?([0-9]*)/)
+        console.log(semver);
+        console.log(`Version: ${version} - Major: ${semver[1]} - Minor: ${semver[2]} - Patch: ${semver[3]}`);
         core.setOutput('version', version);
+        core.setOutput('major', semver[1]);
+        core.setOutput('minor', semver[2]);
+        core.setOutput('patch', semver[3]);
     } catch (error) {
         core.setFailed(error.message);
     }
