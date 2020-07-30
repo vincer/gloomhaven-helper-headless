31fb58c2fef221f013f33f7d174828e6f41d5a5d
vince rosso <vincer@vincer.net>
2020-07-30 14:35:36 -0700

    better regex

--- latest-gloomhaven-helper-action/version.js
+++ latest-gloomhaven-helper-action/version.js
@@ -6,17 +6,15 @@ async function run() {
         const get = bent('http://esotericsoftware.com/gloomhaven-helper');
         const stream = await get();
         const str = await stream.text()
-        const found = str.match(/v[0-9.]+/)
+        const found = str.match(/v([0-9]+)\.([0-9]+)\.?([0-9]*)/)
 
         console.log(`Regex matched: ${found}`);
-        const version = found[0].substr(1);
-        const semver = version.match(/^([0-9]+)\.([0-9]+)\.?([0-9]*)/)
-        console.log(semver);
-        console.log(`Version: ${version} - Major: ${semver[1]} - Minor: ${semver[2]} - Patch: ${semver[3]}`);
+        const version = found[0].substring(1);
+        console.log(`Version: ${version} - Major: ${found[1]} - Minor: ${found[2]} - Patch: ${found[3]}`);
         core.setOutput('version', version);
-        core.setOutput('major', semver[1]);
-        core.setOutput('minor', semver[2]);
-        core.setOutput('patch', semver[3]);
+        core.setOutput('major', found[1]);
+        core.setOutput('minor', found[2]);
+        core.setOutput('patch', found[3]);
     } catch (error) {
         core.setFailed(error.message);
     }
